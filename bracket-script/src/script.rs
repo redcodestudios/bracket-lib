6caa2cf122b970d0ccc57970f9f2d6d790cc4a48
Pablo Diego <pablodiegoss@hotmail.com>
2020-06-23 19:38:51 -0300

    Enable loading multiple scripts from recursive folders

--- bracket-script/src/script.rs
+++ bracket-script/src/script.rs
@@ -23,22 +23,37 @@ impl Script {
         }
     }
 
-    pub fn load_multiple(mut resources: Resources, dir_path: &str) -> Vec<Self> {
-        let mut scripts: Vec<Script> = Vec::new();
-        
+    
+    pub fn load_scripts_from_folder(mut resources: &mut Resources, mut scripts: &mut Vec<Script>, dir_path: &str)-> Vec<Self>{
         if Path::new(dir_path).is_dir() {
             for e in fs::read_dir(dir_path).unwrap() {
-               let entry = e.unwrap();
+                let entry = e.unwrap();
                if !entry.path().is_dir() {
                    let script = Script::new(entry.path().to_str().unwrap());
-                   resources.insert(script.clone());
-                   scripts.push(script.clone());
+                   (*resources).insert(script.clone());
+                   (*scripts).push(script.clone());
+                }else{
+                    Self::load_scripts_from_folder(&mut resources, &mut scripts, entry.path().to_str().unwrap());
                }
             }
         }
+        return scripts.to_vec();
+    }
+    pub fn load_multiple(mut resources: Resources, dir_path: &str) -> Vec<Self> {
+        let mut scripts: Vec<Script> = Vec::new();
+        println!("Loading scripts...");
+        scripts = Self::load_scripts_from_folder(&mut resources,&mut scripts, dir_path);
+
+        if(scripts.is_empty()){
+            println!("No scripts found in folder!");
+        }else{
+            for s in &scripts {
+                println!("\tLoaded: {}", s.path.display());
+            }
+        }
+
         scripts
     }
-
     pub fn to_utf8(self) -> String {
         String::from_utf8(self.bytes).unwrap()
     }
